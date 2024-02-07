29b221f1be0e864d963ad93796ed6de918b9db07
scjpg <shirleyc243@gmail.com>
2024-02-07 18:36:57 -0500

    working on adding shannon H

--- individual log reg analyses.R
+++ individual log reg analyses.R
@@ -1,4 +1,6 @@
 library(readxl)
+library(vegan)
+library(tidyverse)
 
 # load data 
 data_Millien <- read_excel("~/school/BIOL4000/raw data/01_Millien_2023_data/01_Millien_2023_compiled.xlsx")
@@ -6,6 +8,24 @@ View(data_Millien)
 
 data_Ginsberg <- read_excel("~/school/BIOL4000/raw data/02_Ginsberg_2021_data/02_Ginsberg_2021_compiled.xlsx")
 View(data_Ginsberg)
+
+
+
+# add shannon diversity index
+
+# Millien et al. (2023)
+spp_matrix_Millien <- select(data_Millien, 6:14)
+View(spp_matrix_Millien)
+
+spp_H_Millien <- diversity(spp_matrix_Millien)
+spp_H_Millien
+
+data_Millien <- cbind(data_Millien, spp_H = spp_H_Millien) # 0 length error 
+
+length(spp_matrix_Millien) # length 9? interesting...
+print(is.vector(spp_H_Millien)) # TRUE, so it IS a vector for sure
+
+
 
 # logistic regression of tick infection prevalence ~ small mammal richness 
 
