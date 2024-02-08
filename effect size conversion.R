f3ac6d5961f0eb09694bb4a7d9e1b4eebc074faf
scjpg <shirleyc243@gmail.com>
2024-02-07 21:34:19 -0500

    added hedge's g for other papers

--- effect size conversion.R
+++ effect size conversion.R
@@ -25,3 +25,18 @@ esc_rpb(r = -0.08,
         es.type = "g") # g = -1.3076, SE = 0.2897
 
 
+# States et al. (2014)
+exp(0.04) # OR = 1.040811
+
+convert_or2d(or = 1.040811,
+             se = 0.22,
+             totaln = 4,
+             es.type = "g") # g = 0.0126, SE = 0.1213
+
+# Werden et al. (2014)
+exp(-0.41) # OR = 0.6636503
+
+convert_or2d(or = 0.6636503,
+             se = 0.13,
+             totaln = 12,
+             es.type = "g") # g =  -0.2087, SE = 0.0717
