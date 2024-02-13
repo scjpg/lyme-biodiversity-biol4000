4e2561be8c7fd7fd1f0b97090062f6c3a1481722
scjpg <shirleyc243@gmail.com>
2024-02-12 22:02:46 -0500

    corrected for log odds ratio

--- meta analyses.R
+++ meta analyses.R
@@ -8,8 +8,8 @@ all_studies <- read_excel("~/school/BIOL4000/meta analysis values.xlsx")
 studies_rich <- subset(all_studies, meta1 == "1")
 View(studies_rich)
 
-meta_rich <- metagen(TE = OR,
-                 seTE = SE,
+meta_rich <- metagen(TE = lnOR,
+                 seTE = lnSE,
                  studlab = source,
                  data = studies_rich,
                  sm = "OR",
@@ -21,17 +21,18 @@ meta_rich <- metagen(TE = OR,
 summary(meta_rich)
 
 forest(meta_rich, 
-            sortvar = TE,
-            prediction = TRUE, 
-            print.tau2 = FALSE,
-            leftlabs = c("Author (Year)", "OR", "SE"))
+       sortvar = TE,
+       prediction = TRUE, 
+       print.tau2 = FALSE,
+       leftlabs = c("Author (Year)", "OR", "SE"),
+       xlab = "Log Odds Ratio")
 
 # meta using shannon H predictor for studies analyzed independently
 studies_H <- subset(all_studies, meta2 == "1")
 View(studies_H)
 
-meta_H <- metagen(TE = OR,
-                     seTE = SE,
+meta_H <- metagen(TE = lnOR,
+                     seTE = lnSE,
                      studlab = source,
                      data = studies_H,
                      sm = "OR",
@@ -46,15 +47,16 @@ forest(meta_H,
        sortvar = TE,
        prediction = TRUE, 
        print.tau2 = FALSE,
-       leftlabs = c("Author (Year)", "OR", "SE"))
+       leftlabs = c("Author (Year)", "OR", "SE"),
+       xlab = "Log Odds Ratio")
 
 
 # meta using only richness and site 
 studies_onlyrich <- subset(all_studies, biodiversity_metric == "richness" | biodiversity_metric == "site")
 View(studies_onlyrich)
 
-meta_onlyrich <- metagen(TE = OR,
-                  seTE = SE,
+meta_onlyrich <- metagen(TE = lnOR,
+                  seTE = lnSE,
                   studlab = source,
                   data = studies_onlyrich,
                   sm = "OR",
@@ -69,14 +71,15 @@ forest(meta_onlyrich,
        sortvar = TE,
        prediction = TRUE, 
        print.tau2 = FALSE,
-       leftlabs = c("Author (Year)", "OR", "SE"))
+       leftlabs = c("Author (Year)", "OR", "SE"),
+       xlab = "Log Odds Ratio")
 
 # meta using only shannon H and site
 studies_onlyH <- subset(all_studies, biodiversity_metric == "shannon H" | biodiversity_metric == "site")
 View(studies_onlyH)
 
-meta_onlyH <- metagen(TE = OR,
-                         seTE = SE,
+meta_onlyH <- metagen(TE = lnOR,
+                         seTE = lnSE,
                          studlab = source,
                          data = studies_onlyH,
                          sm = "OR",
@@ -91,29 +94,31 @@ forest(meta_onlyH,
        sortvar = TE,
        prediction = TRUE, 
        print.tau2 = FALSE,
-       leftlabs = c("Author (Year)", "OR", "SE"))
+       leftlabs = c("Author (Year)", "OR", "SE"),
+       xlab = "Log Odds Ratio")
 
 # meta without site proxy
 studies_rich_nosite <- subset(studies_rich, biodiversity_metric == "richness" | biodiversity_metric == "shannon H")
 View(studies_rich_nosite)
 
-meta_rich_nosite <- metagen(TE = OR,
-                      seTE = SE,
+meta_rich_nosite <- metagen(TE = lnOR,
+                      seTE = lnSE,
                       studlab = source,
                       data = studies_rich_nosite,
                       sm = "OR",
-                      fixed = FALSE,
+                      fixed = TRUE,
                       random = TRUE,
                       method.tau = "PM",
                       hakn = TRUE)
 
-summary(meta_rich_nosite)
+summary(meta_rich_nosite) # weirdly no between study heterogeneity???
 
 forest(meta_rich_nosite, 
        sortvar = TE,
        prediction = TRUE, 
        print.tau2 = FALSE,
-       leftlabs = c("Author (Year)", "OR", "SE"))
+       leftlabs = c("Author (Year)", "OR", "SE"),
+       xlab = "Log Odds Ratio")
 
 # meta regression w/ scale as covariate
 
@@ -126,15 +131,16 @@ bubble(metareg_scale,
        cex.studlab = 0.6,
        pos.studlab = 4,
        offset = 1,
-       xlab = "Total Sample Site Area (ha)")
+       xlab = "Total Sample Site Area (ha)",
+       ylab = "Log Odds Ratio")
 
 # meta regression w/o LoGiudice et al. (site area estimated)
 
 studies_rich_nosite_noLoGiudice <- subset(studies_rich_nosite, source != "LoGiudice et al. (2008)") 
 View(studies_rich_nosite_noLoGiudice)
 
-meta_rich_nosite_noLoGiudice <- metagen(TE = OR,
-                            seTE = SE,
+meta_rich_nosite_noLoGiudice <- metagen(TE = lnOR,
+                            seTE = lnSE,
                             studlab = source,
                             data = studies_rich_nosite_noLoGiudice,
                             sm = "OR",
@@ -149,7 +155,8 @@ forest(meta_rich_nosite_noLoGiudice,
        sortvar = TE,
        prediction = TRUE, 
        print.tau2 = FALSE,
-       leftlabs = c("Author (Year)", "OR", "SE"))
+       leftlabs = c("Author (Year)", "OR", "SE"),
+       xlab = "Log Odds Ratio")
 
 metareg_noLoGiudice_scale <- metareg(meta_rich_nosite_noLoGiudice, ~ samp_ha)
 summary(metareg_noLoGiudice_scale)
@@ -160,4 +167,5 @@ bubble(metareg_noLoGiudice_scale,
        cex.studlab = 0.6,
        pos.studlab = 2,
        offset = 1,
-       xlab = "Total Sample Site Area (ha)")
+       xlab = "Total Sample Site Area (ha)",
+       ylab = "Log Odds Ratio")
