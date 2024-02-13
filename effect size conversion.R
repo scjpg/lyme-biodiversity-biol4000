ee3b90073270d07a8388a7c25b00c8b31c292b2f
scjpg <shirleyc243@gmail.com>
2024-02-12 19:43:39 -0500

    added log(OR) calculations

--- effect size conversion.R
+++ effect size conversion.R
@@ -26,9 +26,12 @@ esc_rpb(r = -0.08,
 # States et al. (2014) *interaction coefficient* 
 exp(-0.7) # OR = 0.4965853
 
+log(0.4965853) # OR = -0.7
 
 # Werden et al. (2014) *interaction coefficient* 
 exp(0.67) # OR = 1.954237
+
+log(1.954237) # OR = 0.6699998
 
 # Allen et al. (2003)
 sqrt(0.43) # r = -0.6557439, negative because negative relationship
