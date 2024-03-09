ef849779862aa512be6beb0b82fb8d4845862c77
scjpg <shirleyc243@gmail.com>
2024-03-08 20:49:00 -0500

    updated plots

--- individual log reg analyses.R
+++ individual log reg analyses.R
@@ -5,46 +5,34 @@ library(esc)
 
 # load data 
 data_Millien <- read_excel("~/school/BIOL4000/raw data/01_Millien_2023_data/01_Millien_2023_compiled.xlsx")
-View(data_Millien)
 
 data_Ginsberg <- read_excel("~/school/BIOL4000/raw data/02_Ginsberg_2021_data/02_Ginsberg_2021_compiled.xlsx")
-View(data_Ginsberg)
 
 data_Anderson <- read_excel("~/school/BIOL4000/raw data/03_Anderson_2006_data/03_Anderson_2006_compiled.xlsx")
-View(data_Anderson)
 
 
 # add shannon diversity index
 
 # Millien et al. (2023)
 spp_matrix_Millien <- select(data_Millien, 6:14)
-View(spp_matrix_Millien)
 
 spp_H_Millien <- diversity(spp_matrix_Millien)
-spp_H_Millien
 
 data_Millien <- cbind(data_Millien, spp_H = spp_H_Millien)
-View(data_Millien)
 
 # Ginsberg et al. (2021)
 spp_matrix_Ginsberg <- select(data_Ginsberg, 7:18)
-View(spp_matrix_Ginsberg)
 
 spp_H_Ginsberg <- diversity(spp_matrix_Ginsberg)
-spp_H_Ginsberg
 
 data_Ginsberg <- cbind(data_Ginsberg, spp_H = spp_H_Ginsberg)
-View(data_Ginsberg)
 
 # Anderson et al. (2006)
 spp_matrix_Anderson <- select(data_Anderson, 4:10)
-View(spp_matrix_Anderson)
 
 spp_H_Anderson <- diversity(spp_matrix_Anderson)
-spp_H_Anderson
 
 data_Anderson <- cbind(data_Anderson, spp_H = spp_H_Anderson)
-View(data_Anderson)
 
 # logistic regression of tick infection prevalence ~ small mammal richness 
 
@@ -60,7 +48,7 @@ summary(lr_Ginsberg_rich) # beta = -0.2162, SE = 0.6566, p = 0.742
 
 # Anderson et al. (2006)
 lr_Anderson_rich <- glm(formula = prev_pool ~ spp_rich, family = "binomial", data = data_Anderson)
-summary(lr_Anderson_rich) # beta = 0.1656, SE = 0.6339, p = 0.816
+summary(lr_Anderson_rich) # beta = 0.1656, SE = 0.6339, p = 0.794
 
 
 
@@ -78,4 +66,4 @@ summary(lr_Ginsberg_H) # beta = -1.055, SE = 3.016, p = 0.726
 
 # Anderson et al. (2006)
 lr_Anderson_H <- glm(formula = prev_pool ~ spp_H, family = "binomial", data = data_Anderson) 
-summary(lr_Anderson_H) # beta = 0.2682, SE = 6.2746, p = 0.983
+summary(lr_Anderson_H) # beta = 0.2682, SE = 6.2746, p = 0.966
--- meta analyses.R
+++ meta analyses.R
@@ -1,12 +1,13 @@
 library(readxl)
 library(dplyr)
 library(meta)
+library(ggplot2)
+library(metafor)
 
 all_studies <- read_excel("~/school/BIOL4000/meta analysis values.xlsx")
 
 # meta using richness predictor for studies analyzed independently 
 studies_rich <- subset(all_studies, meta1 == "1")
-View(studies_rich)
 
 meta_rich <- metagen(TE = lnOR,
                  seTE = lnSE,
@@ -15,8 +16,9 @@ meta_rich <- metagen(TE = lnOR,
                  sm = "OR",
                  fixed = FALSE,
                  random = TRUE,
-                 method.tau = "PM",
-                 hakn = TRUE)
+                 method.tau = "SJ",
+                 hakn = TRUE,
+                 prediction = TRUE)
 
 summary(meta_rich)
 
@@ -35,44 +37,19 @@ funnel(meta_rich,
        contour = c(0.9, 0.95, 0.99),
        col.contour = col.contour,
        pos.studlab = 4,
-       xlab = "Log Odds Ratio",
-       cex.studlab = 0.6)
-
-legend(x = 0.05, y = 0.05, 
+       xlab = "Odds Ratio",
+       cex.studlab = 0.6) +
+  legend(x = 0.05, y = 0.05, 
        legend = c("p < 0.1", "p < 0.05", "p < 0.01"),
        fill = col.contour)
 
 metabias(meta_rich, 
-         method.bias = "thompson", 
-         k.min = 8)
-
-# meta using shannon H predictor for studies analyzed independently
-studies_H <- subset(all_studies, meta2 == "1")
-View(studies_H)
-
-meta_H <- metagen(TE = lnOR,
-                     seTE = lnSE,
-                     studlab = source,
-                     data = studies_H,
-                     sm = "OR",
-                     fixed = FALSE,
-                     random = TRUE,
-                     method.tau = "PM",
-                     hakn = TRUE)
-
-summary(meta_H)
-
-forest(meta_H, 
-       sortvar = TE,
-       prediction = TRUE, 
-       print.tau2 = FALSE,
-       leftlabs = c("Author (Year)", "OR", "SE"),
-       xlab = "Log Odds Ratio")
-
+         method.bias = "thompson",
+         k.min = 8,
+         plotit = TRUE)
 
 # meta using only richness and site 
 studies_onlyrich <- subset(all_studies, biodiversity_metric == "richness" | biodiversity_metric == "site")
-View(studies_onlyrich)
 
 meta_onlyrich <- metagen(TE = lnOR,
                   seTE = lnSE,
@@ -81,8 +58,9 @@ meta_onlyrich <- metagen(TE = lnOR,
                   sm = "OR",
                   fixed = FALSE,
                   random = TRUE,
-                  method.tau = "PM",
-                  hakn = TRUE)
+                  method.tau = "SJ",
+                  hakn = TRUE,
+                  prediction = TRUE)
 
 summary(meta_onlyrich)
 
@@ -90,12 +68,15 @@ forest(meta_onlyrich,
        sortvar = TE,
        prediction = TRUE, 
        print.tau2 = FALSE,
-       leftlabs = c("Author (Year)", "OR", "SE"),
-       xlab = "Log Odds Ratio")
+       print.I2.ci = TRUE,
+       addrows.below.overall = 3,
+       leftcols = "studlab",
+       leftlabs = "Author (Year)",
+       rightlabs = c("OR", "95% CI", "Weight"),
+       text.random = "Random effects model (K-H)")
 
 # meta using only shannon H and site
 studies_onlyH <- subset(all_studies, biodiversity_metric == "shannon H" | biodiversity_metric == "site")
-View(studies_onlyH)
 
 meta_onlyH <- metagen(TE = lnOR,
                          seTE = lnSE,
@@ -104,8 +85,9 @@ meta_onlyH <- metagen(TE = lnOR,
                          sm = "OR",
                          fixed = FALSE,
                          random = TRUE,
-                         method.tau = "PM",
-                         hakn = TRUE)
+                         method.tau = "SJ",
+                         hakn = TRUE,
+                      prediction = TRUE)
 
 summary(meta_onlyH)
 
@@ -113,45 +95,95 @@ forest(meta_onlyH,
        sortvar = TE,
        prediction = TRUE, 
        print.tau2 = FALSE,
-       leftlabs = c("Author (Year)", "OR", "SE"),
-       xlab = "Log Odds Ratio")
+       print.I2.ci = TRUE,
+       addrows.below.overall = 3,
+       leftcols = "studlab",
+       leftlabs = "Author (Year)",
+       rightlabs = c("OR", "95% CI", "Weight"),
+       text.random = "Random effects model (K-H)")
+
 
 # meta without site proxy
 studies_rich_nosite <- subset(studies_rich, biodiversity_metric == "richness" | biodiversity_metric == "shannon H")
-View(studies_rich_nosite)
 
 meta_rich_nosite <- metagen(TE = lnOR,
                       seTE = lnSE,
                       studlab = source,
                       data = studies_rich_nosite,
                       sm = "OR",
-                      fixed = FALSE,
+                      fixed = TRUE,
                       random = TRUE,
-                      method.tau = "PM",
-                      hakn = TRUE)
+                      method.tau = "SJ",
+                      hakn = TRUE,
+                      prediction = TRUE)
 
 summary(meta_rich_nosite) 
 
+
 forest(meta_rich_nosite, 
+       sortvar = TE,
+       weight.study = "common",
+       prediction = TRUE, 
+       print.tau2 = FALSE,
+       print.I2.ci = TRUE,
+       addrows.below.overall = 3,
+       leftcols = "studlab",
+       leftlabs = "Author (Year)",
+       rightlabs = c("OR", "95% CI", "Weight"),
+       lab.NA.weight = " ",
+       text.w.common = "fixed",
+       text.common = "Fixed effect model",
+       text.random = "Random effects model (K-H)")
+
+# only random effects 
+meta_rich_nosite_r <- metagen(TE = lnOR,
+                            seTE = lnSE,
+                            studlab = source,
+                            data = studies_rich_nosite,
+                            sm = "OR",
+                            fixed = FALSE,
+                            random = TRUE,
+                            method.tau = "SJ",
+                            hakn = TRUE,
+                            prediction = TRUE)
+
+forest(meta_rich_nosite_r, 
        sortvar = TE,
        prediction = TRUE, 
        print.tau2 = FALSE,
-       leftlabs = c("Author (Year)", "OR", "SE"),
-       xlab = "Log Odds Ratio")
+       print.I2.ci = TRUE,
+       addrows.below.overall = 3,
+       leftcols = "studlab",
+       leftlabs = "Author (Year)",
+       rightlabs = c("OR", "95% CI", "Weight"),
+       text.random = "Random effects model (K-H)")
+
+# only richness 
+
+meta_rich_nosite_f <- metagen(TE = lnOR,
+                              seTE = lnSE,
+                              studlab = source,
+                              data = studies_rich_nosite,
+                              sm = "OR",
+                              fixed = FALSE,
+                              random = FALSE,
+                              method.tau = "SJ",
+                              hakn = TRUE,
+                              prediction = TRUE)
 
 # meta regression w/o LoGiudice et al. (no sampling area data)
 studies_rich_nosite_noLoGiudice <- subset(studies_rich_nosite, source != "LoGiudice et al. (2008)") 
-View(studies_rich_nosite_noLoGiudice)
 
 meta_rich_nosite_noLoGiudice <- metagen(TE = lnOR,
                             seTE = lnSE,
                             studlab = source,
                             data = studies_rich_nosite_noLoGiudice,
                             sm = "OR",
-                            fixed = FALSE,
+                            fixed = TRUE,
                             random = TRUE,
-                            method.tau = "PM",
-                            hakn = TRUE)
+                            method.tau = "SJ",
+                            hakn = TRUE,
+                            prediction = TRUE)
 
 summary(meta_rich_nosite_noLoGiudice)
 
@@ -162,14 +194,53 @@ forest(meta_rich_nosite_noLoGiudice,
        leftlabs = c("Author (Year)", "OR", "SE"),
        xlab = "Log Odds Ratio")
 
-metareg_noLoGiudice_scale <- metareg(meta_rich_nosite_noLoGiudice, ~ samp_ha)
+metareg_noLoGiudice_scale <- metareg(meta_rich_nosite_noLoGiudice, ~ samp_ha, 
+                                     method.tau = "SJ",
+                                     hakn = TRUE)
 summary(metareg_noLoGiudice_scale)
 
-bubble(metareg_noLoGiudice_scale, 
-       studlab = TRUE,
-       min.cex = 0.8,
-       cex.studlab = 0.6,
-       pos.studlab = 2,
-       offset = 1,
+# calculating confidence intervals for I^2
+
+Q <- 2.1782
+
+df <- 3
+  
+b = sqrt(1/(2*(df-1)*(1-(1/(3*(df-1)^2)))))
+
+# lower CI
+lower = exp(0.5*log(Q/df)-1.96*b)
+
+lower_limit = ((lower^2)-1)/(lower^2)*100 # -966.8193 --> round up to 0
+
+# upper CI
+upper = exp(0.5*log(Q/df)+1.96*b)
+
+upper_limit = ((upper^2)-1)/(upper^2)*100 # 82.219
+
+# bubble plots
+
+# using meta --> very ugly!
+
+bubble(metareg_noLoGiudice_scale,
+       col.ref = "red",
        xlab = "Total Sample Site Area (ha)",
-       ylab = "Log Odds Ratio")
+       ylab = "Odds Ratio") 
+
+# using ggplot
+
+metareg_df <- data.frame(meta_rich_nosite_noLoGiudice)
+
+metareg_df <- cbind(metareg_df, samp_ha = studies_rich_nosite_noLoGiudice$samp_ha)
+
+ggplot(metareg_df, aes(x = samp_ha, y = TE, size = w.random, colour = studlab, fill = studlab)) +
+  scale_x_continuous(limits = c(5, 55), breaks = c(10, 20, 30, 40, 50)) +
+  scale_y_continuous(limits = c(-1.8, 1.6), breaks = seq(-2, 1.6, 0.4)) +
+  geom_errorbar(aes(x = samp_ha, y = TE, ymin = lower, ymax = upper), size = 1) +
+  geom_point(shape = 21, stroke = 1.5, alpha = 0.5) +
+  scale_size(range = c(5, 13)) +
+  geom_abline(intercept = coef(metareg_noLoGiudice_scale)[1], slope = coef(metareg_noLoGiudice_scale)[2]) +
+  geom_hline(yintercept = 0, colour = "red") +
+  labs(x = "\nTotal Sample Site Area (ha)", y = "Log Odds Ratio\n") +
+  guides(size = FALSE, fill = FALSE) +
+  labs(colour = "Author (Year)") +
+  theme_light()
